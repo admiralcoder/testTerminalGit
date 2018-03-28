cedb25c4f4b940b60d42f76586e3b71ef591d009
Rodriguez <rodriguez@Rodriguezs-iMac.local>
2018-03-28 12:24:06 -0400

    second commit

--- src/test1/HelloWorld.java
+++ src/test1/HelloWorld.java
@@ -1,7 +1,8 @@
 package test1;
 
 public class HelloWorld {
-public static void main(String[] args) {
-	System.out.println("Test Hello World");
-}
+	public static void main(String[] args) {
+		System.out.println("Test Hello World");
+		System.out.println("test 2");
+	}
 }
